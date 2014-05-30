1ff527865ad4b5175b2987a819faee2a4926d546
Luiz Mineo <luiz.mineo@gmail.com>
2014-05-30 09:24:34 -0300

    Updated to Redstone.dart v0.5

--- server/server.dart
+++ server/server.dart
@@ -34,7 +34,7 @@ createConn(DbConnManager connManager) {
     app.chain.next(() => connManager.close(dbConn));
   }).catchError((e) {
     app.chain.interrupt(statusCode: HttpStatus.INTERNAL_SERVER_ERROR, 
-        response: {"error": "DATABASE_UNAVAILABLE"});
+        responseValue: {"error": "DATABASE_UNAVAILABLE"});
   });
 }
 
@@ -157,7 +157,6 @@ main() {
   var portEnv = Platform.environment['PORT'];
 
   app.start(address: '127.0.0.1', 
-            port: portEnv != null ? int.parse(portEnv) : 8080, 
-            staticDir: null);
+            port: portEnv != null ? int.parse(portEnv) : 8080);
 
 }
